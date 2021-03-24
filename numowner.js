b3e15b5a7ce4a80d1277a8e203fd5186351f7753
Guifofoh <76789205+Guifofoh@users.noreply.github.com>
2021-03-23 21:55:53 -0300

    Add files via upload

---
+++ numowner.js
@@ -0,0 +1,12 @@
+const ownerNumber = ["557192165080@s.whatsapp.net"]
+const OriginalOwner = '557192165080'
+const vcard = 'BEGIN:VCARD\n' 
+            + 'VERSION:3.0\n' 
+            + 'FN:Meu criador^~^\n' 
+            + 'ORG:Ian;\n' 
+            + 'TEL;type=CELL;type=VOICE;waid=557192165080:+55 71 9216-5080\n' 
+            + 'END:VCARD'
+
+exports.vcard = vcard
+exports.OriginalOwner = OriginalOwner
+exports.ownerNumber = ownerNumber
